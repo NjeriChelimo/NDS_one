5c7668cbb46ad7a416cf4a8f7653f40b27df7a9f
NjeriChelimo <martha.c.chumo@gmail.com>
2013-09-20 11:00:15 +0300

    started ruby.learncodethehardway.org

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you do 'not'."
+puts 'I said you do not touch this.'
